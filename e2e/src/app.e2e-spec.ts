e3396eb17b2ca7e7e9a1378dc8f03603530351f6
arthurghz <arthurghz01@gmail.com>
2022-04-30 20:38:09 -0300

    ajustados os titulos da aplicação

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('picpay-desafio-frontend app is running!');
+    expect(page.getTitleText()).toEqual('pacdev-front app is running!');
   });
 
   afterEach(async () => {
